bc5ff91ecf1e08d8dc20c17ba2cf20b5e4f83cae
KIM2C1 <119794073+KIM2C1@users.noreply.github.com>
2023-02-12 14:53:25 +0900

    .

--- Practice/Serial.py
+++ Practice/Serial.py
@@ -4,13 +4,13 @@ import time
 ser = serial.Serial("COM9", 9600) # 아두이노의 포트와 보율을 지정합니다.
 try:
     while(1):
-      c = input()
+      c = input("아두이노로 전송할 값: ")
       c = c.encode('utf-8')
       ser.write(c)
 
       time.sleep(1)
       y = ser.readline()
-      print(y.decode('utf-8').strip())
+      print("아두이노에서 받은 값:",y.decode('utf-8').strip())
 
 except KeyboardInterrupt:
     print("중지")
