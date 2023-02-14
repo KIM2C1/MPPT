58218d4f431572c3c24677615c459ea0215703ae
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-02-14 12:17:50 +0900

    .

--- Practice/Serial.py
+++ Practice/Serial.py
@@ -1,21 +1,48 @@
 import serial
 import time
+import crc16
+
+def crc16(data: str, poly: hex = 0xA001) -> str:
+    '''
+        CRC-16 MODBUS HASHING ALGORITHM
+    '''
+    crc = 0xFFFF
+    for byte in data:
+        crc ^= ord(byte)
+        for _ in range(8):
+            crc = ((crc >> 1) ^ poly
+                   if (crc & 0x0001)
+                   else crc >> 1)
+
+    hv = hex(crc).upper()[2:]
+    blueprint = '0000'
+    return (blueprint if len(hv) == 0 else blueprint[:-len(hv)] + hv)
+
+
+""" b = crc16(example_str)
+print(b) """
 
 ser = serial.Serial("COM10", 9600) # 아두이노의 포트와 보율을 지정합니다.
-a =[]
 try:
     while(1):
+      a = []
       c = input("아두이노로 전송할 값: ")
-      c = c.encode('utf-8')
-      ser.write(c)
+      crc_c = crc16(c)
+      cr = '0D'
+      order = c + crc_c + cr
+      print('보낸 값 :', order)
+      order = order.encode('utf-8')
+      ser.write(order)
 
       time.sleep(1)
-      for i in range(len(c)) :
+      for i in range(len(order)) :
           x = ser.readline()
           y = x.decode('utf-8').strip()
           a.append(y)
-      for j in range(len(c)) :
-          print(a[j], end ="")
+          result = ''.join(i for i in a)
+      print("받은 값: ", result)
       print("")
 except KeyboardInterrupt:
     print("중지")
+
+
