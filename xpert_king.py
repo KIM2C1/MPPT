c99341ec0daf9c241ef6d01de7c7d7b50e3b19c8
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-02-17 20:02:11 +0900

    Update xpert_king.py

--- xpert_king.py
+++ xpert_king.py
@@ -2,11 +2,13 @@ import serial
 import time
 import matplotlib
 import numpy
+import copy
+import sys
 ord_byte = []
 ord_byte1 = []
 crc_sum = []
 
-
+ser = serial.Serial('COM6',2400)
 
 def crc16_xmodem(order: bytes) -> int:
     crc = 0
@@ -19,33 +21,44 @@ def crc16_xmodem(order: bytes) -> int:
                 crc <<= 1
     return crc & 0xffff
 
+while True :        
+        ord_byte = []
+        ord_byte1 = []
+        crc_sum = []
+        # ser = serial.Serial(port = 'COM6', baudrate = 9600)
+        order = input('명령어를 입력하세요 : ')
+        order_byte = order.encode('utf-8') # xmodem으로 바꿀 변수
+        len_order = len(order)
+        order = list(order)
+
+        for x in range(len_order) : # 
+            ord_byte1.append(order[x].encode('utf-8'))
+
+        crc = crc16_xmodem(order_byte)
+        crc_hex = hex(crc)
+        crc_hex = list(crc_hex)
+        print('crc_hex', crc_hex) # crc hex 배열 출력
+        cr1 = '\\x' + crc_hex[2] + crc_hex[3] # 배열에서 16진수 1,2 자리 
+        cr1_dc = copy.deepcopy(cr1)
+        print('cr1', cr1)
+        cr2 = '\\x' + crc_hex[4] + crc_hex[5] # 배열에서 16진수 3,4 자리
+        cr2_dc = copy.deepcopy(cr2)  
+        cr1_b = cr1.encode().decode('unicode_escape').encode("raw_unicode_escape")
+        #cr1_b = cr1.encode().decode('unicode_escape').encode('latin1')
+        print(cr1_b)
+        cr2_b = cr2.encode().decode('unicode_escape').encode("raw_unicode_escape")
+        cr = '\r'.encode('utf-8')
+        ord_byte1.append(cr1_b)
+        ord_byte1.append(cr2_b)
+        ord_byte1.append(cr)
 
 
-ser = serial.Serial(port = 'COM6', baudrate = 9600)
-order = input('명령어를 입력하세요 : ')
-order_byte = order.encode('utf-8') # xmodem으로 바꿀 변수
-len_order = len(order)
-order = list(order)
-
-for x in range(len_order) :
-    ord_byte1.append(order[x].encode('utf-8'))
-
-crc = crc16_xmodem(order_byte)
-crc_hex = hex(crc)
-crc_hex = list(crc_hex)
-cr1 = '\\x' + crc_hex[2] + crc_hex[3]
-cr2 = '\\x' + crc_hex[4] + crc_hex[5]  
-cr1_b = cr1.encode().decode('unicode_escape').encode("raw_unicode_escape")
-cr2_b = cr2.encode().decode('unicode_escape').encode("raw_unicode_escape")
-cr = '\r'.encode('utf-8')
-ord_byte1.append(cr1_b)
-ord_byte1.append(cr2_b)
-ord_byte1.append(cr)
-
-
-for y in range(len(ord_byte1)) :
-    ser.write(ord_byte1[y])
-    print(ord_byte1[y])
-
+        for y in range(len(ord_byte1)) :
+            ser.write(ord_byte1[y])
+            print(ord_byte1[y])
+        print('*************************')
+        while True :
+            read = ser.read()
+            print(read)
 
 
